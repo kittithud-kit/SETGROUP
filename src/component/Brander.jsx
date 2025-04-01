98c1dd41f3fb4f67c80e099602e9c0d251ca44ba
kittithud-kit <kittithud25744@gmail.com>
2025-04-01 23:03:25 +0700

    Update Brander.jsx

--- src/component/Brander.jsx
+++ src/component/Brander.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import Flickity from "react-flickity-component";
 import "./Brander.scss";
 
+import logodoh from "/src/assets/images/AboutUs/logo-doh.png";
+import logohww from "/src/assets/images/AboutUs/logo-hww.png";
+import logomtw from "/src/assets/images/AboutUs/logo-mtw.png";
+import logopolice from "/src/assets/images/AboutUs/logo-police.png";
+import logowater from "/src/assets/images/AboutUs/logo-water.png";
+import logotrain from "/src/assets/images/AboutUs/logo-train.png";
+import logobbl from "/src/assets/images/AboutUs/logo-bbl.png";
+import logobjc from "/src/assets/images/AboutUs/logo-bjc.png";
+import logocholpathan from "/src/assets/images/AboutUs/logo-cholpathan.png";
+import logoexat from "/src/assets/images/AboutUs/logo-exat.png";
+import logomajor from "/src/assets/images/AboutUs/logo-major.png";
+import logont from "/src/assets/images/AboutUs/logo-nt.png";
+
 const PageBrandSlider = () => {
   const flickityOptions = {
     contain: true,
@@ -19,47 +32,41 @@ const PageBrandSlider = () => {
       <div className="brands-wrap">
         <Flickity className={"brand-carousel"} options={flickityOptions}>
           <div className="gallery-cell">
-            <img src="/public/images/About Us/logo-doh.png" alt="กรมทางหลวง" />
+            <img src={logodoh} alt="กรมทางหลวง" />
           </div>
           <div className="gallery-cell">
-            <img
-              src="/public/images/About Us/logo-hww.png"
-              alt="สำนักงานควบคุมน้ำหนักยานพาหนะ"
-            />
+            <img src={logohww} alt="สำนักงานควบคุมน้ำหนักยานพาหนะ" />
           </div>
           <div className="gallery-cell">
-            <img src="/public/images/About Us/logo-mtw.png" alt="มอเตอร์เวย์" />
+            <img src={logomtw} alt="มอเตอร์เวย์" />
           </div>
           <div className="gallery-cell">
-            <img
-              src="/public/images/About Us/logo-police.png"
-              alt="การตำรวจภาค7"
-            />
+            <img src={logopolice} alt="การตำรวจภาค7" />
           </div>
           <div className="gallery-cell">
-            <img src="/public/images/About Us/logo-water.png" alt="การปะปา" />
+            <img src={logowater} alt="การปะปา" />
           </div>
           <div className="gallery-cell">
-            <img src="/public/images/About Us/logo-train.png" alt="การรถไฟ" />
+            <img src={logotrain} alt="การรถไฟ" />
           </div>
           <div className="gallery-cell">
-            <img src="/public/images/About Us/logo-bbl.png" alt="" />
+            <img src={logobbl} alt="" />
           </div>
           <div className="gallery-cell">
-            <img src="/public/images/About Us/logo-bjc.png" alt="" />
+            <img src={logobjc} alt="" />
           </div>
 
           <div className="gallery-cell">
-            <img src="/public/images/About Us/logo-cholpathan.png" alt="" />
+            <img src={logocholpathan} alt="" />
           </div>
           <div className="gallery-cell">
-            <img src="/public/images/About Us/logo-exat.png" alt="" />
+            <img src={logoexat} alt="" />
           </div>
           <div className="gallery-cell">
-            <img src="/public/images/About Us/logo-major.png" alt="" />
+            <img src={logomajor} alt="" />
           </div>
           <div className="gallery-cell">
-            <img src="/public/images/About Us/logo-nt.png" alt="" />
+            <img src={logont} alt="" />
           </div>
         </Flickity>
       </div>
