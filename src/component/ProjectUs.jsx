713324da76ecad2b737463fc174b68805ad74513
kittithud-kit <kittithud25744@gmail.com>
2025-04-02 11:55:57 +0700

    Update ProjectUs.jsx

--- src/component/ProjectUs.jsx
+++ src/component/ProjectUs.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./ProjectUs.css";
 
+/* Images */
+import FiberOptic from "../assets/images/Project/Fiber optic มาบตาพุต/ส่งงานดันครั้งที่ 2_190510_0004.jpg";
+
 const ProjectUs = () => {
   return (
     <section id="ProjectUs" className="bg-light-gray">
@@ -26,338 +29,41 @@ const ProjectUs = () => {
                   <i className="fa fa-plus fa-3x"></i>
                 </div>
               </div>
-              <img
-                src="/public/images/Project/CCTV Service Area/LINE_ALBUM_SERVICE AREAS Sta.48+000 R M.7_๒๓๐๖๐๖_18.jpg"
-                className="img-responsive"
-                alt=""
-              />
+              <img src={FiberOptic} className="img-responsive" alt="" />
             </a>
             <div className="ProjectUs-caption">
-              <h4>CCTV Service Area</h4>
+              <h4>Central Lab</h4>
+              <h6 className="projectbath">value 9,999,999 bath</h6>
             </div>
           </div>
 
           {/* Project Modal 2 */}
-          <div className="col-md-4 col-sm-6 ProjectUs-item">
-            <a
-              href="#ProjectUsModal2"
-              className="ProjectUs-link"
-              data-toggle="modal"
-            >
-              <div className="ProjectUs-hover">
-                <div className="ProjectUs-hover-content">
-                  <i className="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                src="/public/images/Project/CCTV ด่านชั่งน้ำหนัก/IMG20190226151349.jpg"
-                className="img-responsive"
-                alt=""
-              />
-            </a>
-            <div className="ProjectUs-caption">
-              <h4>CCTV Weige Station</h4>
-            </div>
-          </div>
 
           {/* Project Modal 3 */}
-          <div className="col-md-4 col-sm-6 ProjectUs-item">
-            <a
-              href="#ProjectUsModal3"
-              className="ProjectUs-link"
-              data-toggle="modal"
-            >
-              <div className="ProjectUs-hover">
-                <div className="ProjectUs-hover-content">
-                  <i className="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                src="/public/images/Project/Kingpin/IMG_8219.JPG"
-                className="img-responsive"
-                alt=""
-              />
-            </a>
-            <div className="ProjectUs-caption">
-              <h4>Kingpin</h4>
-            </div>
-          </div>
 
           {/* Project Modal 4 */}
-          <div className="col-md-4 col-sm-6 ProjectUs-item">
-            <a
-              href="#ProjectUsModal4"
-              className="ProjectUs-link"
-              data-toggle="modal"
-            >
-              <div className="ProjectUs-hover">
-                <div className="ProjectUs-hover-content">
-                  <i className="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                src="/public/images/Project/Central Leb/ความคืบหน้า_๒๑๐๑๐๗_26.jpg"
-                className="img-responsive"
-                alt=""
-              />
-            </a>
-            <div className="ProjectUs-caption">
-              <h4>Central Leb</h4>
-            </div>
-          </div>
 
           {/* Project Modal 5 */}
-          <div className="col-md-4 col-sm-6 ProjectUs-item">
-            <a
-              href="#ProjectUsModal5"
-              className="ProjectUs-link"
-              data-toggle="modal"
-            >
-              <div className="ProjectUs-hover">
-                <div className="ProjectUs-hover-content">
-                  <i className="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                src="/public/images/Project/Fiber optic มาบตาพุต/ส่งงานดันครั้งที่ 2_190510_0004.jpg"
-                className="img-responsive"
-                alt=""
-              />
-            </a>
-            <div className="ProjectUs-caption">
-              <h4>Fiber Optic M7</h4>
-            </div>
-          </div>
 
           {/* Project Modal 6 */}
-          <div className="col-md-4 col-sm-6 ProjectUs-item">
-            <a
-              href="#ProjectUsModal6"
-              className="ProjectUs-link"
-              data-toggle="modal"
-            >
-              <div className="ProjectUs-hover">
-                <div className="ProjectUs-hover-content">
-                  <i className="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                src="/public/images/Project/ระบบบริหารกล้องวงจรปิด/LINE_ALBUM_ระบบบริหารจัดการสำหรับกล้องวงจรปิด_25040.jpg"
-                className="img-responsive"
-                alt=""
-              />
-            </a>
-            <div className="ProjectUs-caption">
-              <h4>CCTV Management System</h4>
-            </div>
-          </div>
 
           {/* Project Modal 7 */}
-          <div className="col-md-4 col-sm-6 ProjectUs-item">
-            <a
-              href="#ProjectUsModal7"
-              className="ProjectUs-link"
-              data-toggle="modal"
-            >
-              <div className="ProjectUs-hover">
-                <div className="ProjectUs-hover-content">
-                  <i className="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                src="https://unsplash.imgix.net/uploads%2F1411419068566071cef10%2F7562527b?q=75&w=1080&h=1080&fit=max&fm=jpg&auto=format&s=240c45655f09c546232a6f106688e502"
-                className="img-responsive"
-                alt=""
-              />
-            </a>
-            <div className="ProjectUs-caption">
-              <h4>Dreams</h4>
-            </div>
-          </div>
 
           {/* Project Modal 8 */}
-          <div className="col-md-4 col-sm-6 ProjectUs-item">
-            <a
-              href="#ProjectUsModal8"
-              className="ProjectUs-link"
-              data-toggle="modal"
-            >
-              <div className="ProjectUs-hover">
-                <div className="ProjectUs-hover-content">
-                  <i className="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                src="https://unsplash.imgix.net/uploads%2F1411419068566071cef10%2F7562527b?q=75&w=1080&h=1080&fit=max&fm=jpg&auto=format&s=240c45655f09c546232a6f106688e502"
-                className="img-responsive"
-                alt=""
-              />
-            </a>
-            <div className="ProjectUs-caption">
-              <h4>Dreams</h4>
-            </div>
-          </div>
 
           {/* Project Modal 9 */}
-          <div className="col-md-4 col-sm-6 ProjectUs-item">
-            <a
-              href="#ProjectUsModal9"
-              className="ProjectUs-link"
-              data-toggle="modal"
-            >
-              <div className="ProjectUs-hover">
-                <div className="ProjectUs-hover-content">
-                  <i className="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                src="https://unsplash.imgix.net/uploads%2F1411419068566071cef10%2F7562527b?q=75&w=1080&h=1080&fit=max&fm=jpg&auto=format&s=240c45655f09c546232a6f106688e502"
-                className="img-responsive"
-                alt=""
-              />
-            </a>
-            <div className="ProjectUs-caption">
-              <h4>Dreams</h4>
-            </div>
-          </div>
 
           {/* Project Modal 10 */}
-          <div className="col-md-4 col-sm-6 ProjectUs-item">
-            <a
-              href="#ProjectUsModal10"
-              className="ProjectUs-link"
-              data-toggle="modal"
-            >
-              <div className="ProjectUs-hover">
-                <div className="ProjectUs-hover-content">
-                  <i className="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                src="https://unsplash.imgix.net/uploads%2F1411419068566071cef10%2F7562527b?q=75&w=1080&h=1080&fit=max&fm=jpg&auto=format&s=240c45655f09c546232a6f106688e502"
-                className="img-responsive"
-                alt=""
-              />
-            </a>
-            <div className="ProjectUs-caption">
-              <h4>Dreams</h4>
-            </div>
-          </div>
 
           {/* Project Modal 11 */}
-          <div className="col-md-4 col-sm-6 ProjectUs-item">
-            <a
-              href="#ProjectUsModal11"
-              className="ProjectUs-link"
-              data-toggle="modal"
-            >
-              <div className="ProjectUs-hover">
-                <div className="ProjectUs-hover-content">
-                  <i className="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                src="https://unsplash.imgix.net/uploads%2F1411419068566071cef10%2F7562527b?q=75&w=1080&h=1080&fit=max&fm=jpg&auto=format&s=240c45655f09c546232a6f106688e502"
-                className="img-responsive"
-                alt=""
-              />
-            </a>
-            <div className="ProjectUs-caption">
-              <h4>Dreams</h4>
-            </div>
-          </div>
 
           {/* Project Modal 12 */}
-          <div className="col-md-4 col-sm-6 ProjectUs-item">
-            <a
-              href="#ProjectUsModal12"
-              className="ProjectUs-link"
-              data-toggle="modal"
-            >
-              <div className="ProjectUs-hover">
-                <div className="ProjectUs-hover-content">
-                  <i className="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                src="https://unsplash.imgix.net/uploads%2F1411419068566071cef10%2F7562527b?q=75&w=1080&h=1080&fit=max&fm=jpg&auto=format&s=240c45655f09c546232a6f106688e502"
-                className="img-responsive"
-                alt=""
-              />
-            </a>
-            <div className="ProjectUs-caption">
-              <h4>Dreams</h4>
-            </div>
-          </div>
 
           {/* Project Modal 13 */}
-          <div className="col-md-4 col-sm-6 ProjectUs-item">
-            <a
-              href="#ProjectUsModal13"
-              className="ProjectUs-link"
-              data-toggle="modal"
-            >
-              <div className="ProjectUs-hover">
-                <div className="ProjectUs-hover-content">
-                  <i className="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                src="https://unsplash.imgix.net/uploads%2F1411419068566071cef10%2F7562527b?q=75&w=1080&h=1080&fit=max&fm=jpg&auto=format&s=240c45655f09c546232a6f106688e502"
-                className="img-responsive"
-                alt=""
-              />
-            </a>
-            <div className="ProjectUs-caption">
-              <h4>Dreams</h4>
-            </div>
-          </div>
 
           {/* Project Modal 14 */}
-          <div className="col-md-4 col-sm-6 ProjectUs-item">
-            <a
-              href="#ProjectUsModal14"
-              className="ProjectUs-link"
-              data-toggle="modal"
-            >
-              <div className="ProjectUs-hover">
-                <div className="ProjectUs-hover-content">
-                  <i className="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                src="https://unsplash.imgix.net/uploads%2F1411419068566071cef10%2F7562527b?q=75&w=1080&h=1080&fit=max&fm=jpg&auto=format&s=240c45655f09c546232a6f106688e502"
-                className="img-responsive"
-                alt=""
-              />
-            </a>
-            <div className="ProjectUs-caption">
-              <h4>Dreams</h4>
-            </div>
-          </div>
 
           {/* Project Modal 15 */}
-          <div className="col-md-4 col-sm-6 ProjectUs-item">
-            <a
-              href="#ProjectUsModal15"
-              className="ProjectUs-link"
-              data-toggle="modal"
-            >
-              <div className="ProjectUs-hover">
-                <div className="ProjectUs-hover-content">
-                  <i className="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                src="https://unsplash.imgix.net/uploads%2F1411419068566071cef10%2F7562527b?q=75&w=1080&h=1080&fit=max&fm=jpg&auto=format&s=240c45655f09c546232a6f106688e502"
-                className="img-responsive"
-                alt=""
-              />
-            </a>
-            <div className="ProjectUs-caption">
-              <h4>Dreams</h4>
-            </div>
-          </div>
         </div>
       </div>
     </section>
